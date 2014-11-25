4d139964f799b4a2d8b08d895bedad9e3fc92440
Peace Da <peace_da@nj-peace-da.client.tw.trendnet.org>
2014-11-25 06:33:50 +0000

    update rm specified files

--- rm_spc_file.py
+++ rm_spc_file.py
@@ -1,30 +1,72 @@
+#!/usr/bin/python
+
 __author__ = 'peace_da'
 
-import sys, os, glob, shutil
+import sys
+import os
+import getopt
+import glob
 
-# for file in glob.glob('*.' + sys.argv[1]):
-# os.remove(file)
+sfxs = []
+path = '.'
+debug = False
+recursive = False
+fls = []
 
-# files = os.listdir('.')
-# print 'file num: ' + str(len(files))
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'p:dr')
+except getopt.GetoptError as ep:
+    print(ep.message)
 
-# for file in os.listdir('.'):
-# root, ext = os.path.split(file)
-# # if ext[1:] == sys.argv[1]:
-# if cmp(ext[1:], sys.argv[1]) == 0:
-# os.remove(file)
+for opt, val in opts:
+    if opt == '-p':
+        path = val
+        if path is None or len(path) == 0:
+            print '-p should offer a path'
+            sys.exit(1)
+    if opt == '-d':
+        debug = True
+    if opt == '-r':
+        recursive = True
 
-def rmFile(surfix, dirname, fnames):
-#    print 'dirname: ', dirname
-#    print 'curent path: ', os.curdir
-    for fname in fnames:
-        base, ext = os.path.splitext(fname)
-        # print 'ext: ', ext
-        if cmp(ext[1:], surfix) == 0:
-            print 'removing file: ', fname
-            # if os.path.exists(file):
-            os.remove(os.path.join(dirname, fname))
+for val in args:
+    sfxs.append(val)
+if len(sfxs) == 0:
+    print 'should offer suffix for specified files'
+    sys.exit(2)
 
-if __name__ == '__main__':
-    os.path.walk(os.getcwd(), rmFile, sys.argv[1])
+if debug is True:
+    print 'opts: ' + str(opts)
+    print 'args: ' + str(args)
+
+
+def rm_fl(local_sfxs, dir_name, fnames):
+    for local_sfx in local_sfxs:
+        local_p = dir_name + '/*.' + local_sfx
+        fls.extend(glob.glob(local_p))
+
+
+if recursive is not True:
+    print 'no recursive'
+    if path[0] == '/':
+        for sfx in sfxs:
+            p = path[0] + '/*.' + sfx
+            fls.extend(glob.glob(p))
+            if debug is True:
+                print 'abs path, match pattern: ' + p
+    else:
+        for sfx in sfxs:
+            p = os.path.join(os.getcwd(), path[0])
+            p += '/*.' + sfx
+            fls.extend(glob.glob(p))
+            if debug is True:
+                print 'rel path, match pattern: ' + p
+else:
+    print 'recursive'
+    os.path.walk(os.getcwd(), rm_fl, sfxs)
+
+for fl in fls:
+    if debug is True:
+        print 'removing file: ' + fl
+    os.remove(fl)
 
