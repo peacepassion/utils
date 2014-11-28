5355731fc98eb7ed19a523e06dc8765dd1b45b9a
Peace Da <peace_da@nj-peace-da.client.tw.trendnet.org>
2014-11-28 11:13:23 +0000

    update sl.py

--- sl.py
+++ sl.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
 
-__author__ = 'peace_da'
-
+from __future__ import print_function
 import sys
 import os
+import fileinput
 
-content = sys.stdin.read(-1)
+# this is a good method using os command
+# content = sys.stdin.read(-1)
+#
+# cmd = "grep --color -n -i ''"
+#
+# stdin = os.popen(cmd, 'w')
+#
+# try:
+# stdin.write(content)
+# finally:
+# stdin.close()
 
-cmd = "grep --color -n -i ''"
 
-stdin = os.popen(cmd, 'w')
-
-try:
-    stdin.write(content)
-finally:
-    stdin.close()
+for line in fileinput.input():
+    print(str(str(fileinput.lineno()) + ':').rjust(3), line, sep=' ', end='')
 
 
 
